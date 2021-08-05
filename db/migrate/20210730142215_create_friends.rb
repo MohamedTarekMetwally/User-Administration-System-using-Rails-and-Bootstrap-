6618fbc8f870673b961b0dac86b827692168d2cc
Mohamed Tarek Metwally <51732423+MohamedTarekMetwally@users.noreply.github.com>
2021-08-06 01:06:09 +0200

    Add files via upload

---
+++ db/migrate/20210730142215_create_friends.rb
@@ -0,0 +1,14 @@
+class CreateFriends < ActiveRecord::Migration[6.1]
+  def change
+    create_table :friends do |t|
+      t.string :first_name
+      t.string :string
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :twitter
+
+      t.timestamps
+    end
+  end
+end
